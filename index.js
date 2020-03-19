689790cf9f5f3c97a2a57963c7cecbe51266c464
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-19 00:01:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-var a = 0
-var b = 0
-
 function add() {
-  var result = a + b
-  return result 
   return "I am a function called add that has no variables or action to be taken."
 }
 
 function substract() {
-  var result = a - b
-  return result 
   return "I am a function called subtract that has no variables or action to be taken."
 }
 
 function multiple() {
-  var result = a * b
-  return result 
   return "I am a function called multiple that has no variables or action to be taken."
 }
 
 function divide() {
-  var result = a / b
-  return result 
   return "I am a function called multiple that has no variables or action to be taken."
 }
 
 function add(a, b) {
-  console.log(a,b)
+  var result = a + b
+  return result 
 }
 
 function subtract(a, b) {
