b137685208accbef67da432b664c6be7f3e23243
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-19 00:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,11 +43,11 @@ function decrement(n) {
 }
 
 function makeInt(n) {
-  result = parseInt(n, 10)
+  var result = parseInt(n, 10)
   return result
 }
 
 function preserveDecimal(n) {
-  result = parseFloat(n)
+  var result = parseFloat(n)
   return result  
 }
